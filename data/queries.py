1aa3ce3574e8cc9766cd01467bf220c1102f8f76
karolinalodzinska <karolinalodzinska98@gmail.com>
2022-11-08 23:06:39 +0100

    Pagination works without highlight the current page

--- data/queries.py
+++ data/queries.py
@@ -5,7 +5,7 @@ def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
 
-def get_most_rated_shows():
+def get_most_rated_shows(page_number):
     return data_manager.execute_select('''
     SELECT shows.id,
     shows.title,
@@ -16,9 +16,14 @@ def get_most_rated_shows():
     shows.trailer,
     shows.homepage
     FROM shows
-    JOIN show_genres ON shows.id = show_genres.show_id
-    JOIN genres ON show_genres.genre_id = genres.id
+    LEFT JOIN show_genres ON shows.id = show_genres.show_id
+    LEFT JOIN genres ON show_genres.genre_id = genres.id
     GROUP BY shows.id
     ORDER BY shows.rating DESC
-    LIMIT 15;
-    ''')
+    LIMIT 15
+    OFFSET ((%(page_number)s -1) * 15);
+    ''', variables={"page_number": page_number})
+
+
+def count_shows_element():
+    return data_manager.execute_select('SELECT count(id) FROM shows;')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 import math
 from dotenv import load_dotenv
@@ -20,8 +20,13 @@ def design():
 
 @app.route('/shows/most-rated')
 def most_rated():
-    shows = queries.get_most_rated_shows()
-    return render_template('most-rated.html', shows=shows)
+    page_number = request.args.get('page', default=1, type=int)
+    shows = queries.get_most_rated_shows(page_number)
+    shows_quantity_dictionary = queries.count_shows_element()
+    shows_quantity = list(shows_quantity_dictionary[0].items())[0][1]
+    max_pagination = math.ceil(shows_quantity/15)
+    return render_template('most-rated.html', shows=shows, page_number=page_number, shows_quantity=shows_quantity,
+                           max_pagination=max_pagination)
 
 
 def main():
