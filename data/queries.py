f7d7099c6fa214b934469900b0f834854bb53575
karolinalodzinska <karolinalodzinska98@gmail.com>
2022-11-08 13:24:11 +0100

    Create query to get most rated shows

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,22 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_most_rated_shows():
+    return data_manager.execute_select('''
+    SELECT shows.id,
+    shows.title,
+    shows.year,
+    shows.runtime,
+    to_char(shows.rating,'999.9') AS rating,
+    string_agg(genres.name,',' ORDER BY genres.name) AS genres,
+    shows.trailer,
+    shows.homepage
+    FROM shows
+    JOIN show_genres ON shows.id = show_genres.show_id
+    JOIN genres ON show_genres.genre_id = genres.id
+    GROUP BY shows.id
+    ORDER BY shows.rating DESC
+    LIMIT 15;
+    ''')
