ceffc0afe35e6f707966ec1aad8f0dc23757cb72
karolinalodzinska <karolinalodzinska98@gmail.com>
2022-11-13 23:35:11 +0100

    Division into smaller functions

--- static/js/actors_list.js
+++ static/js/actors_list.js
@@ -1,29 +1,34 @@
 function getShowsForActor(id) {
     fetch("/actors/" + id).then(response => response.json()).then(function (data) {
-        let displayedShows = document.getElementsByClassName("actor-shows")
-        // usunięcie jeśli klikam w inny element listy
-        if (displayedShows[0] && displayedShows[0].parentNode.id === id.toString()) {
-            for (let i = displayedShows.length - 1; i >= 0; i--) {
-                displayedShows[i].remove()
-            }
-            // usunięcie jeśli klikam w ten sam element listy drugi raz
+        let displayed = document.getElementsByClassName("displayed")
+
+        if (displayed[0] && displayed[0].parentNode.id === id.toString()) {
+            removeElement(displayed)
         } else {
-            if (displayedShows[0]) {
-                for (let i = displayedShows.length - 1; i >= 0; i--) {
-                    displayedShows[i].remove()
-                }
+            if (displayed[0]) {
+                removeElement(displayed)
             }
-            // dodanie elementów
-            for (let i = 0; i < data["actor_list"].length; i++) {
-                let listElement = document.getElementById(id)
-                let ul = document.createElement("ul")
-                ul.classList.add("actor-shows")
-                let li = document.createElement("li")
-                li.innerHTML = data["actor_list"][i]["title"]
-                ul.appendChild(li)
-                listElement.appendChild(ul)
-            }
+            appendElements(id, data)
         }
     })
 }
+
+function appendElements(id, data) {
+    for (let i = 0; i < data['actor_list'].length; i++) {
+        let list = document.getElementById(id)
+        let ul = document.createElement("ul")
+        ul.classList.add("displayed")
+        let li = document.createElement("li")
+        li.innerHTML = data['actor_list'][i]['title']
+        ul.appendChild(li)
+        list.appendChild(ul)
+    }
+}
+
+function removeElement(displayed) {
+    for (let i = displayed.length - 1; i >= 0; i--) {
+        displayed[i].remove()
+    }
+}
+
 
